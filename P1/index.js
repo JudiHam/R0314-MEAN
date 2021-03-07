107c1adfb429b5ccd8c8772d48ed629774449d3e
JudiHam <judi.dasilva@gmail.com>
2021-03-08 00:40:36 +0200

    starting json file parse for table display

--- P1/index.js
+++ P1/index.js
@@ -6,8 +6,6 @@ var express = require("express");
 //Bring body-parser (helps handle form data we get with the POST request)
 var bodyParser = require("body-parser");
 
-const { json } = require("express");
-
 var app = express();
 
 //Activate body-parser
@@ -15,6 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //To serve static content from project directory
 app.use(express.static("./sivut"));
+
+//Read json file into variable
+var file = fs.readFileSync("sivut/messages.json");
+
+//Parsing the file to json.format
+var jsondata = JSON.parse(file);
+
 
 //Create routes
 //1. Frontpage
@@ -24,7 +29,9 @@ app.get("/", function(req, res) {
 
 //2. Guestbook messages page
 app.get("/guestbook", function(req, res) {
-    res.sendFile(__dirname + "\\guestbook.html");
+    //Display json message data into table
+
+    console.log(file);
 });
 
 //3. New message form
@@ -37,16 +44,6 @@ app.get("/ajaxmessage", function(req, res) {
     res.sendFile(__dirname + "\\ajaxmessage.html");
 });
 
-//Read json file into variable
-var file = fs.readFile("sivut/messages.json", 
-  function (err, data) {
-    if (err) {
-      console.log(err);
-    }
-    //File empty in the beginning
-    console.log(data.toString());
-    }
-);
 
 //Next is a route called "sendform" that was declared in the newmessage.html form 
 //and reacts to the POST-type request from the form.
@@ -64,7 +61,12 @@ app.post("/sendform", function(req, res) {
     };
     console.log("newmsg:" + newmsg);
     //Push the object to the json file
-    file.push(newmsg);
+    jsondata.push(newmsg);
+    //Convert to string format 
+    var data = JSON.stringify(jsondata, "", 1);
+    //Add new data to json file
+    fs.writeFileSync("sivut/messages.json", data);
+    res.redirect("newmessage.html");
 });
 
 app.listen(3000, function() {
