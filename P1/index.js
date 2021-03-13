016db7aef8c7422dd2a560bd7b97baf330abcad7
JudiHam <judi.dasilva@gmail.com>
2021-03-13 19:35:12 +0200

    const port added

--- P1/index.js
+++ P1/index.js
@@ -1,3 +1,5 @@
+const PORT = process.env.PORT || 5000;
+
 var fs = require("fs");
 
 // Bring express
@@ -166,7 +168,7 @@ app.get("/ajaxmessage", function(req, res) {
     res.sendFile(__dirname + "\\ajaxmessage.html");
 });
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
     console.log("Example app listening on port 3000!");
 });
 
